f16377c10436d245977cba7a8a2aa0935eaa5bf7
Andreas Tsika <andreastsika@gmail.com>
2022-12-17 03:14:51 +0200

    finished contact form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,64 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// EmailJS Functionality
+document.getElementById('contact-form').addEventListener('submit', emailjs);
+
+function emailjs(event) {
+  event.preventDefault(); // prevent reload
+
+  let form = event.target;
+  let formData = new FormData(form);
+  formData.append('service_id', 'service_ij7vtfc');
+  formData.append('template_id', 'template_mkwvlpk');
+  formData.append('user_id', 'Nz0VCnHh8uuXTW1-w');
+
+  let url = 'https://api.emailjs.com/api/v1.0/email/send-form';
+  let req = new Request(url, {
+    body: formData,
+    method: 'POST',
+  });
+
+  fetch(req)
+    .then((res) => {
+      if (res.ok) {
+        createEmailMessage('success', 'The message was sent successfully.');
+      } else {
+        createEmailMessage(null, 'Sorry something went wrong. (Status: ' + res.status + ') ' + res.statusText);
+      }
+    })
+    .catch((error) => {
+      createEmailMessage('error', error);
+    });
+
+}
+
+function createEmailMessage(type, message) {
+  let cf = document.querySelectorAll('#contact-form')[0];
+  let el = document.createElement('div');
+  
+  el.classList.add('form-status');
+
+  switch(type) {
+    case 'success': 
+      el.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <i class="fa-solid fa-circle-check"></i><span class="mx-3">` + message + `</span>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div>`
+      break;
+    case 'error':
+      el.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <i class="fa-solid fa-circle-exclamation"></i><span class="mx-3">` + message + `</span>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div>`
+      break;
+    default:
+      el.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <i class="fa-solid fa-triangle-exclamation"></i><span class="mx-3">` + message + `</span>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div>`
+  }
+
+  cf.appendChild(el);
+}
